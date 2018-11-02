51100965e954e159ed9403606487301b62945fb5
Siddesh LC <siddeshlc8@gmail.com>
2018-11-02 23:17:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'mysql',
-    database : 'DBMS'
+    host     : '',
+    user     : '',
+    password : '',
+    database : ''
   });
   
 connection.connect(function(err) {
